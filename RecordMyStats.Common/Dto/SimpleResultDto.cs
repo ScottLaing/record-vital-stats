e080b08ec1039f794ed541e020e44dee48ade1c2
Scott Laing <scottlaing@gmail.com>
2024-09-05 17:24:51 -0500

    Code improvements.

--- RecordMyStats.Common/Dto/SimpleResultDto.cs
+++ RecordMyStats.Common/Dto/SimpleResultDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecordMyStats.Common.Dto
+﻿namespace RecordMyStats.Common.Dto
 {
     public class SimpleResultDto
     {
