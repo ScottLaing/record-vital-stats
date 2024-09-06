b5b97d23bccc3ea309fdf047a861f0bd738328d8
Scott Laing <scottlaing@gmail.com>
2024-09-06 07:22:44 -0500

    code improvements.

--- RecordMyStats.Common/Dto/AddEntryDto.cs
+++ RecordMyStats.Common/Dto/AddEntryDto.cs
@@ -1,9 +1,4 @@
 ﻿using RecordMyStats.Common.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RecordMyStats.Common.Dto
 {
--- RecordMyStats.Common/Dto/GetEntriesParamsDto.cs
+++ RecordMyStats.Common/Dto/GetEntriesParamsDto.cs
@@ -1,11 +1,4 @@
-﻿using RecordMyStats.Common.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecordMyStats.Common.Dto
+﻿namespace RecordMyStats.Common.Dto
 {
     public class GetEntriesParamsDto
     {
